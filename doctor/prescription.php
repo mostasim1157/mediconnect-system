0092b4705d28802d728e1b171437ca0deb07caef
TajrianTahrim <88376140+TajrianTahrim@users.noreply.github.com>
2025-08-14 17:23:31 +0600

    Doctors prescription dashboard

---
+++ doctor/prescription.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/animations.css">  
+    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/admin.css">
+        
+    <title>Prescriptions</title>
+    <style>
+        .popup{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+        .sub-table{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+</style>
+</head>
+<body>
+    <?php
+
+    session_start();
+
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
+            header("location: ../login.php");
+        }else{
+            $useremail=$_SESSION["user"];
+        }
+
+    }else{
+        header("location: ../login.php");
+    }
+    
+    include("../connection.php");
+    $userrow = $database->query("select * from doctor where docemail='$useremail'");
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["docid"];
+    $username=$userfetch["docname"];
+    ?>
+    <div class="container">
+        <div class="menu">
+            <table class="menu-container" border="0">
+                <tr>
+                    <td style="padding:10px" colspan="2">
+                        <table border="0" class="profile-container">
+                            <tr>
+                                <td width="30%" style="padding-left:20px" >
+                                    <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
+                                </td>
+                                <td style="padding:0px;margin:0px;">
+                                    <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
+                                    <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft"></a>
+                                </td>
+                            </tr>
+                    </table>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-dashbord" >
+                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
+                    </td>
+                </tr>
+                <tr class="menu-row">
+                    <td class="menu-btn menu-icon-appoinment">
+                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">My Appointments</p></a></div>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-session">
+                        <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">My Sessions</p></div></a>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-patient">
+                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">My Patients</p></a></div>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-dashbord menu-active">
+                        <a href="prescription.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">Prescriptions</p></a></div>
+                    </td>
+                </tr>
+                <tr class="menu-row" >
+                    <td class="menu-btn menu-icon-settings">
+                        <a href="settings.php" class="non-style-link-menu"><div><p class="menu-text">Settings</p></a></div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="dash-body">
+            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
+                <tr >
+                    <td width="13%" >
+                    <a href="index.php" ><button  class="login-btn btn-primary-soft btn btn-icon-back"  style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px"><font class="tn-in-text">Back</font></button></a>
+                    </td>
+                    <td>
+                        <p style="font-size: 23px;padding-left:12px;font-weight: 600;">Prescription Manager</p>
+                                           
+                    </td>
+                    <td width="15%">
+                        <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+                            Today's Date
+                        </p>
+                        <p class="heading-sub12" style="padding: 0;margin: 0;">
+                            <?php 
+                        date_default_timezone_set('Asia/Kolkata');
+
+                        $today = date('Y-m-d');
+                        echo $today;
+                        ?>
+                        </p>
+                    </td>
+                    <td width="10%">
+                        <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="padding-top:10px;width: 100%;" >
+                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All Appointments (<?php
+                            $list11 = $database->query("select appointment.appoid from appointment inner join schedule on schedule.scheduleid=appointment.scheduleid where schedule.docid=$userid;");
+                            echo $list11->num_rows;
+                        ?>)</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="padding-top:0px;width: 100%;" >
+                        <center>
+                        <table class="filter-container" border="0" >
+                        <tr>
+                           <td width="10%">
+                           </td> 
+                        <td width="5%" style="text-align: center;">
+                        Date:
+                        </td>
+                        <td width="30%">
+                        <form action="" method="post">
+                            
+                            <input type="date" name="sheduledate" id="date" class="input-text filter-container-items" style="margin: 0;width: 95%;">
+
+                        </td>
+                        
+                    <td width="12%">
+                        <input type="submit"  name="filter" value=" Filter" class=" btn-primary-soft btn button-icon btn-filter"  style="padding: 15px; margin :0;width:100%">
+                        </form>
+                    </td>
+
+                    </tr>
+                            </table>
+
+                        </center>
+                    </td>
+                    
+                </tr>
+                
+                <?php
+                    $sqlmain= "select appointment.appoid,schedule.scheduleid,schedule.title,doctor.docname,patient.pname,schedule.scheduledate,schedule.scheduletime,appointment.apponum,appointment.pid from schedule inner join appointment on schedule.scheduleid=appointment.scheduleid inner join patient on patient.pid=appointment.pid inner join doctor on schedule.docid=doctor.docid  where  doctor.docid=$userid ";
+
+                    if($_POST){
+                        if(!empty($_POST["sheduledate"])){
+                            $sheduledate=$_POST["sheduledate"];
+                            $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
+                        };
+                    }
+                ?>
+                  
+                <tr>
+                   <td colspan="4">
+                       <center>
+                        <div class="abc scroll">
+                        <table width="93%" class="sub-table scrolldown" border="0">
+                        <thead>
+                        <tr>
+                                <th class="table-headin">
+                                    Patient name
+                                </th>
+                                <th class="table-headin">
+                                    Appointment number
+                                </th>
+                                <th class="table-headin">
+                                    Session Title
+                                </th>
+                                <th class="table-headin">
+                                    Session Date & Time
+                                </th>
+                                <th class="table-headin">
+                                    Events
+                                </tr>
+                        </thead>
+                        <tbody>
+                        
+                            <?php
+                                $result= $database->query($sqlmain);
+
+                                if($result->num_rows==0){
+                                    echo '<tr>
+                                    <td colspan="7">
+                                    <br><br><br><br>
+                                    <center>
+                                    <img src="../img/notfound.svg" width="25%">
+                                    
+                                    <br>
+                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+                                    <a class="non-style-link" href="appointment.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Appointments &nbsp;</font></button>
+                                    </a>
+                                    </center>
+                                    <br><br><br><br>
+                                    </td>
+                                    </tr>';
+                                }
+                                else{
+                                for ( $x=0; $x<$result->num_rows;$x++){
+                                    $row=$result->fetch_assoc();
+                                    $appoid=$row["appoid"];
+                                    $scheduleid=$row["scheduleid"];
+                                    $title=$row["title"];
+                                    $docname=$row["docname"];
+                                    $scheduledate=$row["scheduledate"];
+                                    $scheduletime=$row["scheduletime"];
+                                    $pname=$row["pname"];
+                                    $pid=$row["pid"];
+                                    $apponum=$row["apponum"];
+
+                                    // Check if a prescription exists
+                                    $presc_check = $database->query("select * from prescription where appoid = $appoid");
+
+                                    echo '<tr >
+                                        <td style="font-weight:600;"> &nbsp;'.
+                                        
+                                        substr($pname,0,25)
+                                        .'</td >
+                                        <td style="text-align:center;font-size:23px;font-weight:500; color: var(--btnnicetext);">
+                                        '.$apponum.'
+                                        </td>
+                                        <td>
+                                        '.substr($title,0,15).'
+                                        </td>
+                                        <td style="text-align:center;font-size:12px;">
+                                            '.$scheduledate.'<br>'.$scheduletime.'
+                                        </td>
+                                        <td>
+                                        <div style="display:flex;justify-content: center;">';
+                                    
+                                    if($presc_check->num_rows > 0){
+                                        $presc_row = $presc_check->fetch_assoc();
+                                        $presc_id = $presc_row['presc_id'];
+                                        echo '<a href="?action=view&id='.$presc_id.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>';
+                                    } else {
+                                        echo '<a href="add-prescription.php?appid='.$appoid.'&pid='.$pid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px; background-image: url(\'../img/icons/add.svg\');"><font class="tn-in-text">Add</font></button></a>';
+                                    }
+                                       
+                                    echo '</div>
+                                        </td>
+                                    </tr>';
+                                 }
+                            }
+                            ?>
+                            </tbody>
+                        </table>
+                        </div>
+                        </center>
+                   </td> 
+                </tr>
+            </table>
+        </div>
+    </div>
+    <?php
+    
+    if($_GET){
+        $id=$_GET["id"];
+        $action=$_GET["action"];
+        if($action=='view'){
+            $sqlmain= "select * from prescription where presc_id=?";
+            $stmt = $database->prepare($sqlmain);
+            $stmt->bind_param("i",$id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row=$result->fetch_assoc();
+            $presc_details = $row['presc_details'];
+            $presc_date = $row['presc_date'];
+
+            echo '
+            <div id="popup1" class="overlay">
+                    <div class="popup">
+                    <center>
+                        <h2>Prescription Details</h2>
+                        <a class="close" href="prescription.php">&times;</a>
+                        <div class="content">
+                            <div style="text-align:left; padding: 20px; border: 1px solid #ccc; border-radius: 5px;">
+                                <p><strong>Prescription Date:</strong> '.$presc_date.'</p>
+                                <hr>
+                                <p><strong>Details:</strong></p>
+                                <p>'.nl2br($presc_details).'</p>
+                            </div>
+                        </div>
+                        <div style="display: flex;justify-content: center;">
+                        <a href="prescription.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;OK&nbsp;&nbsp;</font></button></a>
+                        </div>
+                        <br><br>
+                        </center>
+                    </div>
+            </div>
+            ';
+        }
+    }
+    ?>
+</body>
+</html>
